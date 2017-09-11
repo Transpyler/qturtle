eafbadd65259d1bccb4ee750c116c01556b11192
ulyssesBML <ulysses3353@gmail.com>
2017-09-11 20:02:41 -0300

    Starting test of turtlenamespace and fixing qsci_console test

--- src/qturtle/tests/test_qsci_console.py
+++ src/qturtle/tests/test_qsci_console.py
@@ -1,5 +1,4 @@
 import sys
-import os
 import pytest
 import mock
 from qturtle.qsci.qsciconsole import TranspylerConsole
@@ -10,7 +9,6 @@ from qturtle.mainwindow import TurtleMainWindow
 from qturtle.qsci.runner import TranspylerRunner
 
 
-    
 @pytest.fixture
 def m_transpyler():
     trans = Transpyler()
@@ -28,23 +26,33 @@ def transpyler_console(m_transpyler):
     return TranspylerConsole(m_transpyler)
 
 def test__init__(transpyler_console):
-    assert type(transpyler_console) == TranspylerConsole 
+    assert isinstance(transpyler_console,TranspylerConsole) 
 
 def test_getCurrentCommand(transpyler_console):
     assert transpyler_console.getCurrentCommand() == ''
+    transpyler_console._current_command = 'test'
+    assert transpyler_console.getCurrentCommand() == 't\ne\ns\nt\n'
 
-
-def test_keyPressEvent_Return(transpyler_console):
+'''
+def test_inputDialog(qtbot, transpyler_console):
+    import _thread
+    import time, random 
 
     app = QtWidgets.QApplication(sys.argv)
+    with qtbot.waitSignals( transpyler_console.inputDialog('test')):
+        qtbot.addWidget(input_dialog)
+        assert qtbot.waitForWindowShown(inputDialog)
 
-    transpyler_console.isLocked = False
-    transpyler_console._locked_position = (-1000,-1000)
+def test_keyPressEvent_Return(transpyler_console):
+    from collections import deque 
+    app = QtWidgets.QApplication(sys.argv)
+
+    #transpyler_console.isLocked = False
+    #transpyler_console._locked_position = (-1000,-1000)
 
     ev = mock.MagicMock()
     
-    ev.key.return_value =  QtCore.Qt.Key_Return 
+    transpyler_console.background_tasks = deque(True)
+    ev.key.return_value =  QtCore.Qt.Key_Escape
     transpyler_console.keyPressEvent(ev=ev)
-
-    #ev.modifiers.return_value = False
-    #transpyler_console.keyPressEvent(ev=ev)
+'''
---
+++ src/qturtle/tests/test_turtlenamespace.py
@@ -0,0 +1,95 @@
+'''
+
+OBS:
+Every function that had any think related with the atribute scene could not be tested,
+ because the class TurtleScene give seg. fault when it is instaciated
+'''
+
+import sys
+import pytest
+import mock
+
+from PyQt5 import QtWidgets
+from qturtle.turtlenamespace import TurtleNamespaceEnglish, TurtleNamespace
+from transpyler.turtle.qt.scene import TurtleScene 
+
+ # TurtleNamespaceEnglish tests
+
+@pytest.fixture
+def  turtle_namespace_english():
+    return TurtleNamespaceEnglish(scene='test');
+
+def test_init_(turtle_namespace_english):
+    assert isinstance(turtle_namespace_english,TurtleNamespaceEnglish)
+    assert turtle_namespace_english._blacklist == set(turtle_namespace_english._BLACKLIST) 
+    assert turtle_namespace_english._delay == 0.05 
+
+def test__delitem__(turtle_namespace_english):
+    assert turtle_namespace_english._data['back'] != None
+    turtle_namespace_english.__delitem__('back')
+    try:
+        assert turtle_namespace_english._data['back']
+        assert False
+    except KeyError:
+        assert True
+
+def test__getitem__(turtle_namespace_english):
+    assert turtle_namespace_english._data['back'] == turtle_namespace_english.__getitem__('back')
+
+def test__setitem__(turtle_namespace_english):
+    turtle_namespace_english.__setitem__('test',1)
+
+'''
+def test__iter__(turtle_namespace_english):
+    assert turtle_namespace_english.__iter__() == iter(turtle_namespace_english._data)
+'''
+def test__len__(turtle_namespace_english):
+    assert turtle_namespace_english.__len__() == len(turtle_namespace_english._data)
+
+def test__getaliases(turtle_namespace_english):
+    assert turtle_namespace_english._getaliases() == {'back': 'backward',\
+                                                        'bk': 'backward',\
+                                                        'fd': 'forward',\
+                                                        'lt': 'left',\
+                                                        'pd': 'pendown',\
+                                                        'pu': 'penup',\
+                                                        'rt':'right'}
+
+def test_blacklist(turtle_namespace_english):
+   turtle_namespace_english.blacklist('test')
+   assert 'test' in turtle_namespace_english._blacklist
+
+def test_speed(turtle_namespace_english):
+    assert turtle_namespace_english._delay == 0.05
+    turtle_namespace_english.speed(5)
+    assert turtle_namespace_english._delay  == max(0.05/5**2 - 0.0005, 0)
+    
+
+# TurtleNamespace tests
+'''
+@pytest.fixture
+def turtle_namespace():
+    return TurtleNamespace('test')
+
+def test_frente(turtle_namespace):
+turtle_namespace.frente(5) 
+'''
+
+# Other functions
+def test_helpstr():
+    import io
+    import qturtle.turtlenamespace as turtle_namespace
+    stdout, sys.stdout = sys.stdout, io.StringIO()
+    help('def')
+    assert turtle_namespace.helpstr('def') == sys.stdout.getvalue()
+
+def test_function_description():
+    import qturtle.turtlenamespace as turtle_namespace
+    
+    assert(turtle_namespace.function_description('def') == \
+           'A function definition defines a user-defined function object (see -- section The standard type hierarchy):')
+    assert(turtle_namespace.function_description('for') == \
+       "The \"for\" statement is used to iterate over the elements of a sequence -- (such as a string, tuple or list) or other iterable object:")
+    assert(turtle_namespace.function_description('help') == \
+       "help = class _Helper(builtins.object) -- |  Define the builtin \'help\'.")
+
