9d2a6b31be672cd1246f8040f8151f24f5806494
ulyssesBML <ulysses3353@gmail.com>
2017-09-14 18:58:49 -0300

    Starting test of ReplEditor Class

---
+++ src/qturtle/tests/test_repleditor.py
@@ -0,0 +1,22 @@
+import sys
+import pytest
+from qturtle.qsci.qscirepleditor import QsciReplEditor
+from qturtle.qsci.qsciconsole import TranspylerConsole 
+from transpyler import Transpyler
+from PyQt5 import QtWidgets
+
+@pytest.fixture
+def m_transpyler():
+    trans = Transpyler()
+    return trans
+
+@pytest.fixture
+def repleditor(m_transpyler):
+    app = QtWidgets.QApplication(sys.argv)
+    return QsciReplEditor(m_transpyler)
+
+'''
+def test_handleMessageReply(repleditor):
+    repleditor.handleMessageReply("test")
+    assert isinstance(repleditor._console,TranspylerConsole)
+'''
